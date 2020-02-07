031ee646239f56e5ea9e2f3415eefb437d40e9bc
unknown <riponcmt630371@gmail.com>
2020-02-08 00:04:49 +0600

    Desgin add book form 14

--- librarian/add-book.php
+++ librarian/add-book.php
@@ -1,4 +1,24 @@
-<?php require_once "header.php"; ?>
+<?php require_once "header.php";
+
+if(isset($_POST['save_book'])){
+    $book_name = $_POST['book_name'];
+    $book_author_name = $_POST['book_author_name'];
+    $book_purchase_date = $_POST['book_purchase_date'];
+    $book_price = $_POST['book_price'];
+    $book_qty = $_POST['book_qty'];
+    $available_qty = $_POST['available_qty'];
+    $libraian_username = $_POST['libraian_username'];
+
+
+    $image = explode('.',$_FILES['book_image']['name']);
+    $image_ext = end($image);
+    $image = date('Ymdhis .').$image_ext;
+
+    echo $image;
+
+}
+
+?>
                 <!-- content HEADER -->
                 <!-- ========================================================= -->
                 <div class="content-header">
@@ -12,10 +32,76 @@
                 </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
                 <div class="row animated fadeInUp">
-                 
-                 test
+                    <div class="col-sm-6 col-sm-offset-3">
+                        <h4 class="section-subtitle"><b>All add Book </b></h4>
+                            <div class="panel">
+                                <div class="panel-content">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <form class="form-horizontal" method="post" enctype="multipart/form-data">
+                                                <h5 class="mb-lg">To enjoy add Book!</h5>
+                                                <div class="form-group">
+                                                    <label for="book_name" class="col-sm-4 control-label">Book Name</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="text" name="book_name" class="form-control" id="book_name " placeholder="Book Name ">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="book_image" class="col-sm-4 control-label">Book Image </label>
+                                                    <div class="col-sm-8">
+                                                        <input type="file" name="book_image" class="form-control" id="book_image">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="book_author_name" class="col-sm-4 control-label">Book Author Name </label>
+                                                    <div class="col-sm-8">
+                                                        <input type="text" name="book_author_name" class="form-control" id="book_author_name" placeholder="Book author name">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="book_purchase_date" class="col-sm-4 control-label">Book Purchase Date</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="date" name="book_purchase_date" class="form-control" id="book_purchase_date" placeholder="Book purchase date">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="book_price " class="col-sm-4 control-label">Book Price</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="number" name="book_price" class="form-control" id="book_price " placeholder="Book price">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="book_qty" class="col-sm-4 control-label">Book Quantity</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="number" name="book_qty" class="form-control" id="book_qty" placeholder="Book quantity">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="available_qty" class="col-sm-4 control-label">Available Quantity</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="number" name="available_qty" class="form-control" id="available_qty" placeholder="Available quantity">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="libraian_username" class="col-sm-4 control-label">Libraian Username</label>
+                                                    <div class="col-sm-8">
+                                                        <input type="text" name="libraian_username" class="form-control" id="libraian_username" placeholder="Libraian username">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-sm-offset-4 col-sm-8">
+                                                        <button type="submit" name="save_book" class="btn btn-primary"><i class="fa fa-book"></i> Save Book</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
                 </div>
-                <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+                    <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
             </div>
 
 <?php  require_once "footer.php"; ?>
--- librarian/header.php
+++ librarian/header.php
@@ -178,7 +178,7 @@
                                 <!--HOME-->
                                 <li class="<?= $page == 'index.php' ? 'active-item' : ''; ?>"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i><span>Dashboard</span></a></li>
                                 <li class="<?= $page == 'students.php' ? 'active-item' : ''; ?>"><a href="students.php"><i class="fa fa-users" aria-hidden="true"></i><span>Students</span></a></li>
-                                <li class="has-child-item close-item <?= $page == 'manage-books.php' ? 'open-item' : ''; ?><?= $page == 'add-book.php' ? 'open-item' : ''; ?>">
+                                <li class="has-child-item close-item <?= $page == 'manage-books.php' || 'add-book.php' ? 'open-item' : ''; ?>">
                                     <a><i class="fa fa-book" aria-hidden="true"></i><span>Books</span> </a>
                                     <ul class="nav child-nav level-1">
                                         <li class="<?= $page == 'add-book.php' ? 'active-item' : ''; ?>"><a href="add-book.php">Add Book</a></li>
