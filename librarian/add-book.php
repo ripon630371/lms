121a1930fc08351db8371e47cd8f4f648e6e3da4
unknown <riponcmt630371@gmail.com>
2020-02-09 00:22:02 +0600

    Add book information 15 image location

--- librarian/add-book.php
+++ librarian/add-book.php
@@ -18,10 +18,12 @@ if(isset($_POST['save_book'])){
     VALUES ('$book_name','$image','$book_author_name','$book_purchase_date','$book_price','$book_qty','$available_qty','$libraian_username')");
 
     if($result){
-        echo "ok";
+        move_uploaded_file($_FILES['book_image']['tmp_name'],'../images/books/'.$image);
+        $sucess = "Book add sucessfully ";
     }else{
-        echo "No";
+        $error = "Book not add !";
     }
+    
 
 
 }
@@ -42,6 +44,29 @@ if(isset($_POST['save_book'])){
                 <div class="row animated fadeInUp">
                     <div class="col-sm-6 col-sm-offset-3">
                         <h4 class="section-subtitle"><b>All add Book </b></h4>
+
+                        <?php 
+                            if(isset($sucess)){?>
+                                <div class="alert alert-success" role="alert">
+                                    <?php echo $sucess; ?>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            <?php 
+                            }
+                        ?>
+                        <?php 
+                            if(isset($error)){?>
+                                <div class="alert alert-danger" role="alert">
+                                    <?php echo $error; ?>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            <?php 
+                            }
+                        ?>
                             <div class="panel">
                                 <div class="panel-content">
                                     <div class="row">
@@ -51,7 +76,7 @@ if(isset($_POST['save_book'])){
                                                 <div class="form-group">
                                                     <label for="book_name" class="col-sm-4 control-label">Book Name</label>
                                                     <div class="col-sm-8">
-                                                        <input type="text" name="book_name" class="form-control" id="book_name " placeholder="Book Name ">
+                                                        <input type="text" name="book_name" class="form-control" id="book_name " placeholder="Book Name " required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
@@ -63,31 +88,31 @@ if(isset($_POST['save_book'])){
                                                 <div class="form-group">
                                                     <label for="book_author_name" class="col-sm-4 control-label">Book Author Name </label>
                                                     <div class="col-sm-8">
-                                                        <input type="text" name="book_author_name" class="form-control" id="book_author_name" placeholder="Book author name">
+                                                        <input type="text" name="book_author_name" class="form-control" id="book_author_name" placeholder="Book author name" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="book_purchase_date" class="col-sm-4 control-label">Book Purchase Date</label>
                                                     <div class="col-sm-8">
-                                                        <input type="date" name="book_purchase_date" class="form-control" id="book_purchase_date" placeholder="Book purchase date">
+                                                        <input type="date" name="book_purchase_date" class="form-control" id="book_purchase_date" placeholder="Book purchase date" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="book_price " class="col-sm-4 control-label">Book Price</label>
                                                     <div class="col-sm-8">
-                                                        <input type="number" name="book_price" class="form-control" id="book_price " placeholder="Book price">
+                                                        <input type="number" name="book_price" class="form-control" id="book_price " placeholder="Book price" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="book_qty" class="col-sm-4 control-label">Book Quantity</label>
                                                     <div class="col-sm-8">
-                                                        <input type="number" name="book_qty" class="form-control" id="book_qty" placeholder="Book quantity">
+                                                        <input type="number" name="book_qty" class="form-control" id="book_qty" placeholder="Book quantity" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="available_qty" class="col-sm-4 control-label">Available Quantity</label>
                                                     <div class="col-sm-8">
-                                                        <input type="number" name="available_qty" class="form-control" id="available_qty" placeholder="Available quantity">
+                                                        <input type="number" name="available_qty" class="form-control" id="available_qty" placeholder="Available quantity" required>
                                                     </div>
                                                 </div>
                                                
