3cf31941db3c782d66ac8c8485c1c2ed81a15156
unknown <riponcmt630371@gmail.com>
2020-02-10 00:22:46 +0600

    Quick view book 17

--- librarian/manage-books.php
+++ librarian/manage-books.php
@@ -12,10 +12,114 @@
                 </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
                 <div class="row animated fadeInUp">
-                 
-                 test
-                </div>
+                    <div class="col-sm-12">
+                            <h4 class="section-subtitle"><b>All Students</b></h4>
+                                <div class="panel">
+                                    <div class="panel-content">
+                                        <div class="table-responsive">
+                                            <table id="basic-table" class="table-bordered data-table table table-striped nowrap table-hover" cellspacing="0" width="100%">
+                                                <thead>
+                                                <tr>
+                                                    <th>Book Name</th>
+                                                    <th>Book Image</th>
+                                                    <th>Author Name</th>
+                                                    <th>Purchase Date</th>
+                                                    <th>Book Price</th>
+                                                    <th>Book Quantity</th>
+                                                    <th>Availale Quantity</th>
+                                                    <th>Libraian Username</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <?php
+                                                        $result = mysqli_query($con, "SELECT * FROM `books`");
+                                                        while($row = mysqli_fetch_assoc($result)){ ?>
+                                                            <tr>
+                                                                <td><?= $row['book_name'];?></td>
+                                                                <td><img src="../images/books/<?= $row['book_image'];?>" alt="" style="width: 50px;"></td>
+                                                                <td><?= $row['book_author_name'];?></td>
+                                                                <td><?= $row['book_purchase_date'];?></td>
+                                                                <td><?= $row['book_price'];?></td>
+                                                                <td><?= $row['book_qty'];?></td>
+                                                                <td><?= $row['available_qty'];?></td>
+                                                                <td><?= $row['libraian_username'];?></td>
+                                                                <td>
+                                                                    <a href="javascript:void(0)" class="btn btn-info" data-toggle="modal" data-target="#book-<?= $row['id'];?>"><i class="fa fa-eye"></i></a>
+                                                                    <a href="javascript:void(0)" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+                                                                    <a href="javascript:void(0)" class="btn btn-danger"><i class="fa fa-trash-o"></i></a>
+                                                                </td>
+                                                            </tr>
+                                                        <?php 
+                                                        }
+                                                    ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
             </div>
-
+            <?php
+                $result = mysqli_query($con, "SELECT * FROM `books`");
+                while($row = mysqli_fetch_assoc($result)){ 
+            ?>                                               
+    <!-- Modal -->
+        <div class="modal fade" id="book-<?= $row['id'];?>" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header state modal-info">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modal-info-label"><i class="fa fa-book"></i>Book Info</h4>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Book Name</th>
+                                <td><?= $row['book_name'];?></<td>
+                            </tr>
+                            <tr>
+                                <th>Book Image</th>
+                                <td><img src="../images/books/<?= $row['book_image'];?>" alt="" style="width: 50px;"></td>                  
+                            </tr>
+                            <tr>
+                                <th>Author Name</th>
+                                <td><?= $row['book_author_name'];?></td>
+                            </tr>
+                            <tr>
+                                <th>Purchase Date</th>
+                                <td><?= $row['book_purchase_date'];?></td>
+                            </tr>
+                            <tr>
+                                <th>Book Price</th>
+                                <td><?= $row['book_price'];?></td>
+                            </tr>
+                            <tr>  
+                                <th>Book Quantity</th>
+                                <td><?= $row['book_qty'];?></td>
+                            </tr>
+                            <tr>
+                                <th>Availale Quantity</th>
+                                <td><?= $row['available_qty'];?></td>                
+                            </tr>  
+                            <tr>
+                                <th>Libraian Username</th>
+                                <td><?= $row['libraian_username'];?></td>
+                            </tr>                   
+                                                  
+                        </table>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php 
+             }
+        ?>
 <?php  require_once "footer.php"; ?>
--- librarian/students.php
+++ librarian/students.php
@@ -12,60 +12,60 @@
                 </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
                 <div class="row animated fadeInUp">
-                <div class="col-sm-12">
-                    <h4 class="section-subtitle"><b>All Students</b></h4>
-                        <div class="panel">
-                            <div class="panel-content">
-                                <div class="table-responsive">
-                                    <table id="basic-table" class="data-table table table-striped nowrap table-hover" cellspacing="0" width="100%">
-                                        <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Roll</th>
-                                            <th>Reg.No</th>
-                                            <th>Email</th>
-                                            <th>Username</th>
-                                            <th>Phone</th>
-                                            <th>Image</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
-                                        </tr>
-                                        </thead>
+                    <div class="col-sm-12">
+                        <h4 class="section-subtitle"><b>All Students</b></h4>
+                            <div class="panel">
+                                <div class="panel-content">
+                                    <div class="table-responsive">
+                                        <table id="basic-table" class="data-table table table-striped nowrap table-hover" cellspacing="0" width="100%">
+                                            <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Roll</th>
+                                                <th>Reg.No</th>
+                                                <th>Email</th>
+                                                <th>Username</th>
+                                                <th>Phone</th>
+                                                <th>Image</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            </thead>
 
-                                        <tbody>
-                                            <?php
-                                                $result = mysqli_query($con, "SELECT * FROM `students`");
-                                                while($row = mysqli_fetch_assoc($result)){ ?>
-                                                    <tr>
-                                                        <td><?= ucwords($row['fname'].' '.$row['lname']); ?></td>
-                                                        <td><?= $row['reg']; ?></td>
-                                                        <td><?= $row['roll']; ?></td>
-                                                        <td><?= $row['reg']; ?></td>
-                                                        <td><?= $row['email']; ?></td>
-                                                        <td><?= $row['username']; ?></td>
-                                                        <td><img src="<?= $row['image']; ?>" alt=""></td>
-                                                        <td><?= $row['status'] == 1 ? 'Active' : 'Inactive'; ?></td>
-                                                        <td>
-                                                            <?php
-                                                                if($row['status']== 1){ ?>
-                                                                    <a href="status_inactive.php?id=<?= base64_encode($row['id']); ?>" class="btn btn-primary"><i class="fa fa-arrow-up"></i></a>
+                                            <tbody>
+                                                <?php
+                                                    $result = mysqli_query($con, "SELECT * FROM `students`");
+                                                    while($row = mysqli_fetch_assoc($result)){ ?>
+                                                        <tr>
+                                                            <td><?= ucwords($row['fname'].' '.$row['lname']); ?></td>
+                                                            <td><?= $row['reg']; ?></td>
+                                                            <td><?= $row['roll']; ?></td>
+                                                            <td><?= $row['reg']; ?></td>
+                                                            <td><?= $row['email']; ?></td>
+                                                            <td><?= $row['username']; ?></td>
+                                                            <td><img src="<?= $row['image']; ?>" alt=""></td>
+                                                            <td><?= $row['status'] == 1 ? 'Active' : 'Inactive'; ?></td>
+                                                            <td>
                                                                 <?php
-                                                                }else{ ?>
-                                                                    <a href="status_active.php?id=<?= base64_encode($row['id']); ?>" class="btn btn-danger"><i class="fa fa-arrow-down"></i></a>
-                                                                <?php }
-                                                            ?>              
-                                                        </td>
-                                                    </tr>
-                                                <?php 
-                                                }
-                                            ?>
-                                        </tbody>
-                                    </table>
+                                                                    if($row['status']== 1){ ?>
+                                                                        <a href="status_inactive.php?id=<?= base64_encode($row['id']); ?>" class="btn btn-primary"><i class="fa fa-arrow-up"></i></a>
+                                                                    <?php
+                                                                    }else{ ?>
+                                                                        <a href="status_active.php?id=<?= base64_encode($row['id']); ?>" class="btn btn-danger"><i class="fa fa-arrow-down"></i></a>
+                                                                    <?php }
+                                                                ?>              
+                                                            </td>
+                                                        </tr>
+                                                    <?php 
+                                                    }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
             </div>
 
