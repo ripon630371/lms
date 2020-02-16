047a8f73fda89db8e71b58fc9415057c251a51a1
unknown <riponcmt630371@gmail.com>
2020-02-16 22:34:12 +0600

    Update book information 19

--- librarian/manage-books.php
+++ librarian/manage-books.php
@@ -10,6 +10,9 @@
                         </ul>
                     </div>
                 </div>
+
+
+
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
                 <div class="row animated fadeInUp">
                     <div class="col-sm-12">
@@ -47,7 +50,7 @@
                                                                 <td><?= $row['libraian_username'];?></td>
                                                                 <td>
                                                                     <a href="javascript:void(0)" class="btn btn-info" data-toggle="modal" data-target="#book-<?= $row['id'];?>"><i class="fa fa-eye"></i></a>
-                                                                    <a href="javascript:void(0)" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+                                                                    <a href="javascript:void(0)" class="btn btn-warning" data-toggle="modal" data-target="#book-update-<?= $row['id'];?>"><i class="fa fa-pencil"></i></a>
                                                                     <a href="delete.php?bookdelete=<?= base64_encode($row['id']);?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete me!')"><i class="fa fa-trash-o"></i></a>
                                                                 </td>
                                                             </tr>
@@ -64,6 +67,9 @@
                     </div>
                 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
             </div>
+
+
+
             <?php
                 $result = mysqli_query($con, "SELECT * FROM `books`");
                 while($row = mysqli_fetch_assoc($result)){ 
@@ -115,6 +121,98 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php 
+             }
+        ?>
+
+
+
+
+<!--update book model here-->
+            <?php
+                $result = mysqli_query($con, "SELECT * FROM `books`");
+                while($row = mysqli_fetch_assoc($result)){ 
+                
+                $id = $row['id'];
+
+                $book_info = mysqli_query($con, "SELECT * FROM `books` WHERE `id` = '$id'");
+                $book_info_row = mysqli_fetch_assoc($book_info);
+
+            ?>                                               
+    <!-- Modal -->
+        <div class="modal fade" id="book-update-<?= $row['id'];?>" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header state modal-info">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modal-info-label"><i class="fa fa-book"></i>Update Book Info</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel">
+                            <div class="panel-content">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form class="form-horizontal" method="post" enctype="multipart/form-data">
+                                            <div class="form-group">
+                                                <label for="book_name" class="col-sm-4 control-label">Book Name</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="book_name" class="form-control" id="book_name " placeholder="Book Name " required value="<?= $book_info_row['book_name'];?>">
+                                                </div>
+                                            </div>
+
+                                            <!--<div class="form-group">
+                                                <label for="book_image" class="col-sm-4 control-label">Book Image </label>
+                                                <div class="col-sm-8">
+                                                    <input type="file" name="book_image" class="form-control" id="book_image">
+                                                </div>
+                                            </div>-->
+
+                                            <div class="form-group">
+                                                <label for="book_author_name" class="col-sm-4 control-label">Book Author Name </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="book_author_name" class="form-control" id="book_author_name" placeholder="Book author name" required value="<?= $book_info_row['book_author_name'];?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_purchase_date" class="col-sm-4 control-label">Book Purchase Date</label>
+                                                <div class="col-sm-8">
+                                                    <input type="date" name="book_purchase_date" class="form-control" id="book_purchase_date" placeholder="Book purchase date" required value="<?= $book_info_row['book_purchase_date'];?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_price " class="col-sm-4 control-label">Book Price</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" name="book_price" class="form-control" id="book_price " placeholder="Book price" required value="<?= $book_info_row['book_price'];?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_qty" class="col-sm-4 control-label">Book Quantity</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" name="book_qty" class="form-control" id="book_qty" placeholder="Book quantity" required value="<?= $book_info_row['book_qty'];?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="available_qty" class="col-sm-4 control-label">Available Quantity</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" name="available_qty" class="form-control" id="available_qty" placeholder="Available quantity" required value="<?= $book_info_row['available_qty'];?>">
+                                                </div>
+                                            </div>
+                                            
+
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-4 col-sm-8">
+                                                    <button type="submit" name="update_book" class="btn btn-primary"><i class="fa fa-save"></i> Update Book</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
